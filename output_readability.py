886404a24951a4a9224b84ac3854780384b05d61
Talya <talya.jeter@duke.edu>
2023-01-27 12:08:59 -0500

    adding classworking py files

---
+++ output_readability.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 27 12:08:05 2023
+
+@author: lilbl
+"""
+
+weight = 20 / 2.205
+dosage = weight * 30
+
+print("CORRECT DOSAGE")
+print("For a patient weighing {} kg,".format(weight))
+print("  the correct dosage is {} mg the first day".format(dosage))
+
