88ad0c2d9871cffc173e7e0d435ee1503e1f9117
Talya <talya.jeter@duke.edu>
2023-02-01 12:41:55 -0500

    able to retrieve patient entry from database based on medical record number

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ Created on Wed Feb  1 12:18:19 2023
 
 @author: lilbl
 """
-
+#%%
 def create_patient_entry(patient_name, patient_mrn,patient_age):
     new_patient=[patient_name,patient_mrn,patient_age]
     return new_patient
@@ -14,6 +14,19 @@ def main_driver():
     db.append(create_patient_entry("Bob Boyles",2,45))
     db.append(create_patient_entry("Chris Chou",3,52))
     print(db)
-    
+    print("Get patient Ann: ")
+    mrn_to_find=4
+    found_patient=get_patient_entry(db, mrn_to_find)#this function knows what db is, so you don't have to return it
+    if found_patinet is False: 
+        print("Patient mrn {} not found").format(mrn_to_find)
+    else: 
+        print(found_patient)
+def get_patient_entry(db,mrn_to_find): #this function doesn't know the variable db, 
+#but it knows to call whatever the first entry to it db 
+    for patient in db:
+        if patient[1] == mrn_to_find:
+            return patient
+    return False
+#%%    
 if __name__=='__main__':
     main_driver()
