e9cf42947e3a093eeab0d523f08f3dca5bfb5083
Talya <talya.jeter@duke.edu>
2023-05-03 14:11:21 -0400

    fixed some things

--- test_slope.py
+++ test_slope.py
@@ -6,11 +6,11 @@ Created on Wed Feb  8 12:03:34 2023
 """
 import pytest
 
-@pytest.mark.parametrize("xs", "ys", "newx", "expected",
-                         [((0,1), (0,1), 2, 2),
-                          ((1,2), (2,4), 2, 4),
-                          ((2,5), (3,9), -2, -11),
-                          ((2,5), (4,6), 1, 4.5)])
+@pytest.mark.parametrize("xs, ys, newx, expected",
+                         [([0,1], [0,1], 2, 2),
+                          ([1,2], [2,4], 2, 4),
+                          ([2,5], [3,9], -2, -11),
+                          ([2,5], [4,6], 1, 4.5)])
 def test_slope(xs, ys, newx, expected):
     from check_slope import slope
     answer=slope(xs, ys, newx)
